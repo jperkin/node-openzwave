42172591b3bc6e7b08bdd8016026503ad85ad7dc
Jonathan Perkin <jonathan@perkin.org.uk>
2013-10-13 21:30:14 +0100

    Update the test program to print node values.
    Also include some minor output changes.

--- test.js
+++ test.js
@@ -5,37 +5,62 @@
 var OZW = require('./lib/openzwave.js').Emitter;
 
 var zwave = new OZW();
+var nodes = [];
 
 zwave.on('connected', function() {
 	console.log('connected');
 
 	zwave.on('driver ready', function() {
-		console.log('driver ready');
+		console.log('scanning...');
 	});
 
 	zwave.on('node added', function(nodeid) {
-		console.log('new node discovered: ' + nodeid);
+		nodes[nodeid] = {
+			manufacturer: '',
+			product: '',
+			type: '',
+			loc: '',
+			values: {},
+		};
+	});
+
+	zwave.on('value added', function(nodeid, type, value) {
+		nodes[nodeid]['values'][type] = value;
+	});
+
+	zwave.on('value changed', function(nodeid, type, value) {
+		if (nodes[nodeid]['values'][type] != value) {
+			console.log('node%d: %s=%s->%s', nodeid, type,
+				    nodes[nodeid]['values'][type], value);
+			nodes[nodeid]['values'][type] = value;
+		}
 	});
 
 	zwave.on('node ready', function(nodeid, nodeinfo) {
-		console.log('node ' + nodeid + ' ready: (' +
-			    nodeinfo.manufacturer + ' ' +
-			    nodeinfo.product + ', ' +
-			    nodeinfo.type + ', ' +
-			    nodeinfo.location + ')');
+		nodes[nodeid]['manufacturer'] = nodeinfo.manufacturer;
+		nodes[nodeid]['product'] = nodeinfo.product;
+		nodes[nodeid]['type'] = nodeinfo.type;
+		nodes[nodeid]['loc'] = nodeinfo.loc;
+		console.log('node%d: %s, %s', nodeid,
+			    nodeinfo.manufacturer,
+			    nodeinfo.product);
+		console.log('node%d: type="%s", location="%s"', nodeid,
+			    nodeinfo.type,
+			    nodeinfo.loc);
+		for (val in nodes[nodeid]['values']) {
+			console.log('node%d: %s=%s', nodeid, val, nodes[nodeid]['values'][val]);
+		}
 	});
 
 	zwave.on('scan complete', function() {
-		console.log('scan complete');
+		console.log('scan complete, hit ^C to finish.');
 	});
 });
 
 zwave.connect();
 
 process.on('SIGINT', function() {
-	console.log('disconnecting');
+	console.log('disconnecting...');
 	zwave.disconnect();
 	process.exit();
 });
-
-console.log('running');
