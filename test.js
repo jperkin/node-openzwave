ee1b7ff966f136fb634fb4ca01f960e723b0b2e8
Jonathan Perkin <jonathan@perkin.org.uk>
2013-10-19 23:20:37 +0100

    Fix homeid output.
    Fixes part of issue #8.

--- test.js
+++ test.js
@@ -10,7 +10,7 @@ var zwave = new OpenZWave('/dev/ttyUSB0', {
 var nodes = [];
 
 zwave.on('driver ready', function(homeid) {
-	console.log('scanning homeid=0x%x...', homeid);
+	console.log('scanning homeid=0x%s...', homeid.toString(16));
 });
 
 zwave.on('driver failed', function() {
